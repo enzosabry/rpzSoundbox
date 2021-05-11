7e6be71bbaa7d8eab4a1ccbecc1415994723f533
Remy Pallabot <r_pallabot@hotmail.fr>
2021-05-11 10:58:02 +0200

    Initial commit, Home page

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from "./src/screens/Home";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
