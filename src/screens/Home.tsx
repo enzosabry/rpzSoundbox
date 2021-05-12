3b6baceba8fe9c720c4a246ad63ac4107bdcc8a7
Remy Pallabot <r_pallabot@hotmail.fr>
2021-05-12 11:18:02 +0200

    add text and image

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Text, View, Image} from "react-native";
+
 
 export default function Home() {
-    return(
+
+    const styles = StyleSheet.create({
+        container: {
+            paddingTop: 50,
+        },
+        tinyLogo: {
+            width: 50,
+            height: 50,
+        },
+        logo: {
+            width: 66,
+            height: 58,
+        },
+    });
+
+    return (
         <View>
-            <Text>
-                Home
-            </Text>
+            <Text>RPZ SoundBox</Text>
+            <Image
+                style={styles.tinyLogo}
+                source={require('../../assets/img/logorpz.png')}
+            />
+
+            <View>
+                <Text>Choose Category</Text>
+            </View>
         </View>
     );
+
+
+
 }
