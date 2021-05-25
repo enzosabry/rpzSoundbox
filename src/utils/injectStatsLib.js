6c5eeb3c2ec0a278cefb2338c0aabed7c3f73872
Enzo Houadef <enzosabry@live.fr>
2021-05-25 03:40:29 +0200

    https

--- src/utils/injectStatsLib.js
+++ src/utils/injectStatsLib.js
@@ -3,7 +3,7 @@ let text = "";
 try {
     const d = fs.readFileSync("docs/index.html", "utf-8");
     text = d.substr(0, 37) +
-        `<script async defer data-website-id="38833760-e45d-4d24-806d-c66c02980267" src="http://enzosabry.fr:12345/umami.js"></script>` +
+        `<script async defer data-website-id="38833760-e45d-4d24-806d-c66c02980267" src="https://umami.enzosabry.fr/umami.js"></script>` +
         d.substr(37);
     fs.writeFileSync("docs/index.html", text);
 } catch(e) {
